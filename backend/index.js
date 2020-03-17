4fdaf5dcf9d5057d5d9b37fc317689bd5a8837d8
Dmitry Verteyko <verteyko1990@gmail.com>
2020-03-17 22:17:40 +0200

    firsts api requests

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,46 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {
+        id: 1,
+        name: 'course1'
+    },
+    {
+        id: 2,
+        name: 'course2'
+    },    {
+        id: 3,
+        name: 'course3'
+    },
+
+]
+
 app.get('/', (req,res) => {
     // request and response
-    res.send('Hello wertey');
+    res.send('Hello Dmitry');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3,4,5]);
+    res.send(courses);
 });
 
-app.listen(3001, () => {
-    console.log('listening on port 3001');
+app.get('/api/courses/:id', (req, res) => {
+    const course =  courses.find((c) => {
+      return c.id === parseInt(req.params.id)
+    })
+    if (!course) res.status(404).send('the course with the given ID was not find');
+    res.send(course);
+    // res.send(req.params.id);
+    // two properties
+    // '/api/courses/:id/:year
+    // res.send(req.params)
+    // '/api/courses/:id/:year?sortBy=name
+    //res.send(req.query)
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
