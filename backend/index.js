676531f921b244c12a54054b42cd9e32cb08c619
Dmitry Verteyko <verteyko1990@gmail.com>
2020-03-17 23:04:11 +0200

    firsts api requests

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
+const joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     {
@@ -37,10 +40,64 @@ app.get('/api/courses/:id', (req, res) => {
     // res.send(req.params)
     // '/api/courses/:id/:year?sortBy=name
     //res.send(req.query)
-})
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
+
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req,res) => {
+    const course =  courses.find((c) => {
+        return c.id === parseInt(req.params.id)
+    });
+    if (!course) res.status(404).send('the course with the given ID was not find');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course)
+});
+
+app.put('/api/courses/:id', (req,res) => {
+    const course =  courses.find((c) => {
+        return c.id === parseInt(req.params.id)
+    })
+    if (!course) res.status(404).send('the course with the given ID was not find');
+
+    const { error } = validateCourse(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+})
+
+function validateCourse(course) {
+    const schema = {
+        name: joi.string().min(3).required(),
+    };
+    return  joi.validate(course, schema);
+}
