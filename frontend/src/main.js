ad49caae93ed6670e91646d2d5162300191ce3a5
Dmitry Verteyko <verteyko1990@gmail.com>
2020-03-24 21:27:22 +0200

    validation and sign up & login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+Vue.use(Vuelidate);
 Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,74 +1,79 @@
 <template>
-  <div class="home">
+  <form @submit.prevent="submit">
     {{ users }}
-    <div class="home-text">
-      {{ newUser }}
-      <label class="home-text__label">Email</label>
-      <input class="home-text__input"
-             v-model="newUser.email"
-             type="email"
-             placeholder="Email">
+    <div class="form-group" :class="{ 'form-group--error': $v.email.$error }">
+      <label class="form__label">Email</label>
+      <input class="form__input" v-model.trim="$v.email.$model"/>
     </div>
-    <div class="home-text">
-      <label class="home-text__label">Password</label>
-      <input class="home-text__input"
-             v-model="newUser.password"
-             type="password"
-             placeholder="Password">
+    <div class="error" v-if="!$v.email.required">Email is required</div>
+    <div class="error" v-if="!$v.email.minLength">Email must have at least
+      {{$v.email.$params.minLength.min}} letters.</div>
+    <div class="form-group" :class="{ 'form-group--error': $v.password.$error }">
+      <label class="form__label">Password</label>
+      <input class="form__input" v-model.trim="$v.password.$model"/>
     </div>
-    <div class="home-button">
-      <button type="button"
-              class="home-button__btn"
-              @click="signUp()"
-      >Sign up</button>
-    </div>
-    <br>
-    <button type="button"
-            class="home-button__btn"
-            @click="logIn()"
-    >Log in</button>
-  </div>
+    <div class="error" v-if="!$v.password.required">Password is required</div>
+    <div class="error" v-if="!$v.password.minLength">Password must have at least
+      {{$v.password.$params.minLength.min}} letters.</div>
+    <button class="button" type="submit" :disabled="submitStatus === 'PENDING'">Submit!</button>
+    <p class="typo__p" v-if="submitStatus === 'OK'">Thanks for your submission!</p>
+    <p class="typo__p" v-if="submitStatus === 'ERROR'">Please fill the form correctly.</p>
+    <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
+  </form>
 </template>
 
 <script>
+import { required, minLength } from 'vuelidate/lib/validators';
 import axios from 'axios';
 
 export default {
-  name: 'Home',
   data() {
     return {
-      newUser: {
-        email: null,
-        password: null,
-      },
+      password: '',
+      email: '',
+      submitStatus: null,
       users: [],
     };
   },
-  methods: {
-    signUp() {
-      fetch('http://localhost:3000/api/auth/login', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          email: this.newUser.email,
-          password: this.newUser.password,
-        }),
-      });
+  validations: {
+    email: {
+      required,
+      minLength: minLength(3),
     },
-    logIn() {
-      const self = this;
-      fetch('http://localhost:3000/users/login', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          email: this.newUser.email,
-          password: this.newUser.password,
-        }),
-      }).then((response) => {
-        if (response.status === '200') {
-          self.$router.push('/wertey');
-        }
-      });
+    password: {
+      required,
+      minLength: minLength(3),
+    },
+  },
+  methods: {
+    submit() {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR';
+      } else {
+        this.submitStatus = 'PENDING';
+        // log in
+        // fetch('http://localhost:3000/users/login', {
+        //   method: 'post',
+        //   headers: { 'Content-Type': 'application/json' },
+        //   body: JSON.stringify({
+        //     email: this.email,
+        //     password: this.password,
+        //   }),
+        // });
+        // sign up
+        fetch('http://localhost:3000/api/auth/login', {
+          method: 'post',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            email: this.email,
+            password: this.password,
+          }),
+        });
+        setTimeout(() => {
+          this.submitStatus = 'OK';
+        }, 500);
+      }
     },
   },
   mounted() {
@@ -76,47 +81,8 @@ export default {
       .get('http://localhost:3000/api/database')
       .then((response) => {
         this.users = response.data;
-      })
-      .catch((err) => console.log(err));
+      }).catch((err) => console.log(err));
   },
 };
+
 </script>
-
-
-<style lang="scss" scoped>
-  .home {
-    max-width: 600px;
-    margin: 0 auto;
-    padding: 20px;
-    &-button {
-      margin-top: 15px;
-      &__btn {
-        width: 100%;
-        height: 40px;
-        background: blueviolet;
-        color: white;
-        border: none;
-        transition: opacity .3s ease-in;
-        cursor: pointer;
-        &:hover {
-          opacity: .8;
-        }
-      }
-    }
-    &-text {
-      &__label {
-        display: block;
-        text-align: left;
-        margin: 15px 0;
-      }
-      &__input {
-        width: 100%;
-        height: 40px;
-        padding-left: 20px;
-        padding-right: 10px;
-        font-size: 20px;
-        box-sizing: border-box;
-      }
-    }
-  }
-</style>
